aac1ac0fde9994a727250fc10b499315a5701871
Fernando Quispe Su <fernando.qsu@gmail.com>
2023-10-24 03:03:25 -0500

    first commit

---
+++ dist/main.js
@@ -0,0 +1,26 @@
+function nav() {
+    return {
+        navOpen: false,
+        active: "home",
+        searchBar:false,
+        
+    }
+}
+
+function dropdown() {
+    return {
+        open:false,
+    }
+}
+
+function drawer() {
+    return {
+        drawerOpen:false,
+    }
+}
+
+function accordion() {
+    return {
+        selected: null
+    }
+}
